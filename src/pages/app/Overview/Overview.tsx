32f3d3acbbae8b8d4599e55f37561d7a3bfd7a23
Aswin Asok <aswinasokofficial@gmail.com>
2024-01-20 12:30:22 +0530

    ui(overview):header-made

--- src/pages/app/Overview/Overview.tsx
+++ src/pages/app/Overview/Overview.tsx
@@ -1,9 +1,25 @@
-import styles from "./Overview.module.css"
+import Theme from "../../../components/Theme/Theme";
+import styles from "./Overview.module.css";
 
 const Overview = () => {
-  return (
-    <div>Overview</div>
-  )
-}
+    return (
+        <Theme>
+            <div className={styles.overViewContainer}>
+                <div className={styles.headerRow}>
+                    <p className={styles.headerText}>
+                        <img
+                            className={styles.headerImage}
+                            src="/scale.webp"
+                            alt=""
+                        />
+                        Scale Up Conclave 2024
+                    </p>
+                    <p className={styles.date}>2-3 Feb 2024</p>
+                </div>
+                <hr className={styles.line} />
+            </div>
+        </Theme>
+    );
+};
 
-export default Overview
+export default Overview;
