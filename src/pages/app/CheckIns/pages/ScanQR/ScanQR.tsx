fa3eeb0b9f49c51991def893a8b80f0086c4504c
Aswin Asok <aswinasokofficial@gmail.com>
2024-01-24 20:49:28 +0530

    ui(scanner):scanner-added

--- src/pages/app/CheckIns/pages/ScanQR/ScanQR.tsx
+++ src/pages/app/CheckIns/pages/ScanQR/ScanQR.tsx
@@ -3,7 +3,12 @@ import CheckInHeader from "../../components/CheckInHeader/CheckInHeader/CheckInH
 import styles from "./ScanQR.module.css";
 import { HiOutlineCamera } from "react-icons/hi2";
 
+import { QrScanner } from "@yudiel/react-qr-scanner";
+import { useState } from "react";
+
 const ScanQR = () => {
+    const [showQR, setShowQR] = useState(false);
+
     return (
         <Theme>
             <div className={styles.scanContainer}>
@@ -15,22 +20,46 @@ const ScanQR = () => {
             <div className={styles.scannerContainer}>
                 <p className={styles.scanHeader}>Scan QR Code Below</p>
 
-                <div className={styles.scanner}>
-                    <div className={styles.cameraAlert}>
-                        <div className={styles.camerabox}>
-                            <HiOutlineCamera size={35} color="#5B75FB" />
-                        </div>
+                {!showQR ? (
+                    <div className={styles.scannerr}>
+                        <div className={styles.cameraAlert}>
+                            <div
+                                onClick={() => {
+                                    setShowQR(true);
+                                }}
+                                className={styles.camerabox}
+                            >
+                                <HiOutlineCamera size={35} color="#5B75FB" />
+                            </div>
 
-                        <div className={styles.alertTexts}>
-                            <p className={styles.alertHeading}>
-                                Please Enable Camera Access
-                            </p>
-                            <p className={styles.alertText}>
-                                We need access to your camera to scan QR codes.
-                            </p>
+                            <div className={styles.alertTexts}>
+                                <p className={styles.alertHeading}>
+                                    Click The Icon & Please Enable Camera Access
+                                </p>
+                                <p className={styles.alertText}>
+                                    We need access to your camera to scan QR
+                                    codes.
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                ) : (
+                    <div className={styles.scannerOuterContainer}>
+                        <div className={styles.scanner}>
+                            <QrScanner
+                                containerStyle={{
+                                    backgroundColor: "#000",
+                                }}
+                                onResult={(result) => {
+                                    console.log(result);
+                                }}
+                                onError={(error) => {
+                                    console.log(error);
+                                }}
+                            />
+                        </div>
+                    </div>
+                )}
             </div>
         </Theme>
     );
