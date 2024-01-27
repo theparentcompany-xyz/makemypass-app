e597b63aee3d5e7aa4e401cc0dcb08468926d996
Aswin Asok <aswinasokofficial@gmail.com>
2024-01-27 18:31:53 +0530

    style(landing-page):responsive-improvements

--- src/pages/app/LandingPage/LandingPage.tsx
+++ src/pages/app/LandingPage/LandingPage.tsx
@@ -18,20 +18,20 @@ const LandingPage = () => {
       <Theme type='landing'>
         <div className={styles.landingPageContainer}>
           <div className={styles.headerSection}>
-            <div className={styles.roundThing}>One Click, One Scanb</div>
+            <div className={styles.roundThing}>One Click, One Scan</div>
             <div className={styles.headingText}>
               <p className={styles.mainHeading}>
                 <span>Make</span> Your <br /> Passes <span>with Ease</span>
               </p>
               <p className={styles.subHeading}>
-                Streamline your pass experience with Make My Pass—a minimalist SASS app for
-                creating, managing, and sharing digital passes effortlessly.
+                Streamline your pass experience with Make My Pass, a minimalist app for creating,
+                managing, and sharing digital passes effortlessly.
               </p>
 
               <button className={styles.launch}>
                 Lauching Soon <span>🚀</span>
               </button>
-              <p className={styles.launchAt}>ScaleUp Conclave 2024</p>
+              <p className={styles.launchAt}>@ ScaleUp Conclave 2024</p>
 
               <img className={styles.landingImage} src='/app/landingImage.png' />
             </div>
