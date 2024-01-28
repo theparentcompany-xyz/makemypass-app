51fb6148285ad4c98087505969d913490ac68b4f
Aswin Asok <aswinasokofficial@gmail.com>
2024-01-28 16:06:46 +0530

    stack-user

--- src/pages/app/InEventStats/InEventStats.tsx
+++ src/pages/app/InEventStats/InEventStats.tsx
@@ -18,6 +18,11 @@ import {
 import { Line, Bar } from 'react-chartjs-2';
 import { useEffect, useState } from 'react';
 import { ChartData } from '../Insights/types';
+import { guests } from '../Guests/types';
+import { useParams } from 'react-router-dom';
+import { connectPrivateSocket } from '../../../../services/apiGateway';
+import { makeMyPassSocket } from '../../../../services/urls';
+import { getEventId } from '../../../apis/events';
 
 ChartJS.register(
   CategoryScale,
@@ -46,6 +51,55 @@ const InEventStats = () => {
 
   const [lineData, setLineData] = useState<ChartData>();
   const [barData, setBarData] = useState<ChartData>();
+
+  const [guests, setGuests] = useState<guests[]>([]);
+  const getLocalEventId = () => {
+    if (eventTitle) {
+      const eventData = JSON.parse(localStorage.getItem('eventData') as string);
+
+      if (eventData) {
+        if (eventData.event_name !== eventTitle) {
+          localStorage.removeItem('eventData');
+          getEventId(eventTitle);
+        } else {
+          return eventData.event_id;
+        }
+      }
+    }
+  };
+
+  const { eventTitle } = useParams<{ eventTitle: string }>();
+  const eventId = getLocalEventId();
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+  useEffect(() => {
+    if (eventId)
+      connectPrivateSocket({
+        url: makeMyPassSocket.listCheckinGuests(eventId),
+      }).then((ws) => {
+        ws.onmessage = (event) => {
+          if (JSON.parse(event.data).response.datas)
+            setGuests(JSON.parse(event.data).response.datas);
+          else if (JSON.parse(event.data).response.data) {
+            const newGuest = JSON.parse(event.data).response.data;
+
+            setGuests((prev) => {
+              const updatedGuests = [newGuest, ...prev];
+
+              return updatedGuests;
+            });
+          }
+        };
+
+        setSocket(ws);
+      });
+  }, [eventId]);
+
+  useEffect(() => {
+    return () => {
+      socket?.close();
+    };
+  }, []);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -154,39 +208,29 @@ const InEventStats = () => {
             }}
             className={styles.pageVisitsCount}
           >
+            <p className={styles.header}>Recent Check-Ins</p>
             <div className={styles.countSection}>
-              <div className={styles.cLeftSection}>
-                <div className={styles.totalRegistered}>
-                  <p className={styles.total}>Page Visits</p>
-                  <p className={styles.count}>
-                    100
-                    <span>Visits</span>
-                  </p>
-                </div>
-                <div className={styles.weeklyCounts}>
-                  <div className={styles.weeklyCount}>
-                    <p className={styles.week}>Yesterday</p>
-                    <p className={styles.wcount}>510</p>
-                  </div>
-                  <div className={styles.weeklyCount}>
-                    <p className={styles.week}>This week</p>
-                    <p className={styles.wcount}>5200</p>
-                  </div>
-                </div>
-                <div className={styles.liveTraffic}>
-                  <p className={styles.live}>Conversion Rate Vs Page Visit</p>
-                  <p className={styles.lcount}>51%</p>
-                </div>
-              </div>
-              <div className={styles.cRightSection}>
-                <p className={styles.rightSectionHeading}>Registration Cities</p>
-
-                <div className={styles.categories}>
-                  <div className={styles.category}>
-                    <p className={styles.categoryName}>Student</p>
-                    <p className={styles.categoryCount}>100%</p>
-                  </div>
-                </div>
+              <div className={styles.usersContainer}>
+                {guests &&
+                  guests.length > 0 &&
+                  guests.map((guest, index) => (
+                    <div
+                      key={index}
+                      className={styles.user}
+                      style={
+                        index === 0
+                          ? {
+                              background: 'rgba(31, 185, 31, 0.1)',
+                            }
+                          : {
+                              border: '1px solid rgba(255, 255, 255, 0.04)',
+                              background: 'rgba(255, 255, 255, 0.04)',
+                            }
+                      }
+                    >
+                      <p className={styles.userName}>{guest.name}</p>
+                    </div>
+                  ))}
               </div>
             </div>
           </div>
