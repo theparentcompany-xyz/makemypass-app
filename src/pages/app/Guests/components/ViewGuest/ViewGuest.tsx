cdd881a560653bb3c44d97af9ec2a3ada961a8d6
Aswin <aswinasokofficial@gmail.com>
2024-02-21 21:35:08 +0530

    feat(guests):mannual-checkin-added

--- src/pages/app/Guests/components/ViewGuest/ViewGuest.tsx
+++ src/pages/app/Guests/components/ViewGuest/ViewGuest.tsx
@@ -7,6 +7,8 @@ import { shortListUser } from '../../../../../apis/guest';
 import { AnimatePresence, motion } from 'framer-motion';
 import { BsTicketPerforatedFill } from 'react-icons/bs';
 import { MdDownload } from 'react-icons/md';
+import { FaCheck } from 'react-icons/fa6';
+import { checkInUser } from '../../../../../apis/scan';
 
 const ViewGuest = ({
   formFields,
@@ -46,7 +48,10 @@ const ViewGuest = ({
           <div className={styles.row}>
             <div className={styles.tsTexts}>
               <p className={styles.name}>
-                {formData['name']}
+                <span>
+                  {formData['name']}{' '}
+                  <span className={styles.rowType}>{formData['ticket_code']}</span>
+                </span>
                 {formData['is_shortlisted'] && <span className={styles.rowType}>Shortlisted</span>}
               </p>
               <p className={styles.emailAddress}>{formData['email']}</p>
@@ -54,7 +59,12 @@ const ViewGuest = ({
             <div className={styles.type}>Students</div>
           </div>
           <div className={styles.tsRow2}>
-            <div>
+            <div
+              className={styles.row}
+              style={{
+                justifyContent: 'flex-start',
+              }}
+            >
               <div className={styles.field}>
                 <p className={styles.fieldLabel}>Registered</p>
                 <p className={styles.fieldData}>{formData['registered_at']}</p>
@@ -66,39 +76,50 @@ const ViewGuest = ({
                 </div>
               )}
             </div>
-            <div className={styles.guestActions}>
-              {!confirmClicked.confirm && formData['is_shortlisted'] ? (
-                <SecondaryButton
-                  onClick={() => {
-                    setConfirmClicked({
-                      confirm: true,
-                      value: false,
-                    });
-                  }}
-                  buttonText='Decline'
-                />
-              ) : (
-                !confirmClicked.confirm && (
-                  <SecondaryButton
-                    onClick={() => {
-                      setConfirmClicked({
-                        confirm: true,
-                        value: true,
-                      });
-                    }}
-                    buttonText='Accept'
-                  />
-                )
-              )}
-              <AnimatePresence>
+            <AnimatePresence>
+              <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 50 }}
+                className={styles.guestActions}
+              >
+                <div className={styles.confirmButton}>
+                  {!confirmClicked.confirm && formData['is_shortlisted'] ? (
+                    <SecondaryButton
+                      onClick={() => {
+                        setConfirmClicked({
+                          confirm: true,
+                          value: false,
+                        });
+                      }}
+                      buttonText='Decline'
+                    />
+                  ) : (
+                    !confirmClicked.confirm && (
+                      <SecondaryButton
+                        onClick={() => {
+                          setConfirmClicked({
+                            confirm: true,
+                            value: true,
+                          });
+                        }}
+                        buttonText='Accept'
+                      />
+                    )
+                  )}
+                </div>
+
                 {confirmClicked.confirm && (
-                  <motion.div
-                    initial={{ x: -10, opacity: 0 }}
-                    animate={{ x: 0, opacity: 1 }}
-                    exit={{ x: -10, opacity: 0 }}
-                    transition={{ duration: 0.5 }}
-                    className={styles.confirmButton}
-                  >
+                  <div className={styles.confirmButton}>
+                    <SecondaryButton
+                      onClick={() => {
+                        setConfirmClicked({
+                          confirm: false,
+                          value: false,
+                        });
+                      }}
+                      buttonText='No'
+                    />
                     <SecondaryButton
                       onClick={() => {
                         shortListUser(
@@ -115,10 +136,10 @@ const ViewGuest = ({
                         ? 'Are you sure you want to accept this guest?'
                         : 'Are you sure you want to decline this guest?'}
                     </p>
-                  </motion.div>
+                  </div>
                 )}
-              </AnimatePresence>
-            </div>
+              </motion.div>
+            </AnimatePresence>
           </div>
           <div className={styles.guestActionButtons}>
             <div
@@ -134,7 +155,14 @@ const ViewGuest = ({
                 }
               }}
             >
-              <BsTicketPerforatedFill title='Resend Ticket' color='#8E8E8E' />
+              <BsTicketPerforatedFill
+                style={{
+                  marginRight: '5px',
+                }}
+                size={20}
+                title='Resend Ticket'
+                color='#8E8E8E'
+              />
               <span>Resent Ticket</span>
             </div>
             <div
@@ -149,9 +177,25 @@ const ViewGuest = ({
                 }
               }}
             >
-              <MdDownload color='#8E8E8E' />
+              <MdDownload size={20} color='#8E8E8E' />
               <span>Download Ticket</span>
             </div>
+            {!formData['check_in_date'] && (
+              <div
+                onClick={() => {
+                  checkInUser(formData['ticket_code'], eventId);
+                  setSelectedGuestId((prevState) => ({
+                    ...prevState,
+                    id: '',
+                    type: '',
+                  }));
+                }}
+                className={styles.icon}
+              >
+                <FaCheck size={20} color='#8E8E8E' />
+                <span>Check-In User</span>
+              </div>
+            )}
           </div>
         </div>
         <hr className={styles.line} />
