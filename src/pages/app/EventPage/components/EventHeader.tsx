fd4296e3b094c6dd6c448e57f53b4781bd7d2eb7
Aswin Asok <aswinasokofficial@gmail.com>
2024-03-02 16:44:04 +0530

    added-desc

--- src/pages/app/EventPage/components/EventHeader.tsx
+++ src/pages/app/EventPage/components/EventHeader.tsx
@@ -67,7 +67,99 @@ const EventHeader = ({ eventData }: { eventData: EventDetails | undefined }) => 
                 animate={{ height: showFullDesc ? 'fit-content' : '60px' }}
                 transition={{ duration: 0.5 }}
               >
-                {showFullDesc ? eventData?.description : eventData?.description.slice(0, 200)}
+                {showFullDesc ? (
+                  <>
+                    <p>
+                      <strong>Building for Billions - Dawn of DPI</strong> is an event hosted by the
+                      Sunbird community, bringing together practitioners from diverse backgrounds to
+                      share their valuable experiences in building Digital Public Goods (DPGs) and
+                      Digital Public Infrastructure (DPI) using Open-Source technology.
+                    </p>
+
+                    <p>
+                      <strong>Event Objectives:</strong>
+                    </p>
+                    <ul>
+                      <li>
+                        Create awareness and grow the community around Digital Public Goods (DPGs)
+                        and Sunbird Building Blocks by showcasing the next-generation tech stacks
+                        and architectural concepts.
+                      </li>
+                      <li>
+                        Illustrate the business opportunities that are opening up for organizations,
+                        entrepreneurs, and other businesses in the field of technology, thanks to
+                        the advent of DPI.
+                      </li>
+                      <li>
+                        Discuss and brainstorm with the developer communities, the key concepts
+                        around - architecture for scale and reliability, interoperability,
+                        configurability etc. This would also provide tangible learning for the
+                        participants.
+                      </li>
+                    </ul>
+
+                    <p>
+                      An exciting chance to deliberate on and explore business opportunities in
+                      technology, this is a first-of-its kind event in Trivandrum that will host
+                      some of the most eminent personalities in the technology domain in India
+                      today.
+                    </p>
+
+                    <p>
+                      This will also be an opportunity to engage with experts who have built tech
+                      systems at national scale, a chance to expand your knowledge, and to gain
+                      insights to play a crucial role in shaping the future of inclusive digital
+                      innovation.
+                    </p>
+
+                    <p>
+                      <strong>Event Details:</strong>
+                    </p>
+                    <p>
+                      <strong>Date:</strong> 14th March, 2024
+                    </p>
+                    <p>
+                      <strong>Location:</strong> Concert hall - Kerala Arts & Crafts Village,
+                      Thiruvananthapuram
+                    </p>
+                    <p>
+                      <strong>Time:</strong> 9:00 AM to 6:00 PM
+                    </p>
+                    <p>
+                      <strong>Google Map:</strong>{' '}
+                      <a href='https://maps.app.goo.gl/ktRQxFSZidAi8yxX8'>View Location</a>
+                    </p>
+
+                    <p>
+                      <strong>Event Experience:</strong>
+                    </p>
+                    <ul>
+                      <li>
+                        <strong>Discover</strong> - Insights by industry leaders, DPI and DPG
+                        pioneers.
+                      </li>
+                      <li>
+                        <strong>Explore</strong> - exhibits of diverse adoptions of Sunbird and
+                        other DPGs.
+                      </li>
+                      <li>
+                        <strong>Learn</strong> - about DPGs and OpenSource products presented by
+                        practitioners, about the exciting business opportunities in the field, and
+                        the prospects of collaboration.
+                      </li>
+                      <li>
+                        <strong>Contribute</strong> - share ideas, feedback, practical experiences
+                        and collaborate to brainstorm innovative solutions leveraging DPGs.
+                      </li>
+                      <li>
+                        <strong>Transform</strong> - Participants will have the opportunity to get
+                        the required knowledge to showcase their innovative solutions.
+                      </li>
+                    </ul>
+                  </>
+                ) : (
+                  eventData?.description.slice(0, 200)
+                )}
               </motion.p>
               <div className={styles.expandIcon}>
                 <FaExpandAlt
