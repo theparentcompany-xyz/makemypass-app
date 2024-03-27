8542f2fefd172be00fa11ddd683af24d2da98bd1
Aswin Asok <aswinasokofficial@gmail.com>
2024-03-27 09:19:10 +0530

    fix(landing):projects-order-changed

--- src/pages/app/LandingPage/components/Projects/Projects.tsx
+++ src/pages/app/LandingPage/components/Projects/Projects.tsx
@@ -93,8 +93,8 @@ const Projects = ({ events }: { events: PubllicEvent[] }) => {
         <div className={styles.projectsListing}>
           <div className={styles.project}>
             <div className={styles.projectDateDay}>
-              <p className={styles.projectDate}>2-3rd Feb 2024</p>
-              <p className={styles.projectDay}>Friday</p>
+              <p className={styles.projectDate}>14th March 2024</p>
+              <p className={styles.projectDay}>Thursday</p>
             </div>
             <div className={styles.lineDot}>
               <div className={styles.dot}></div>
@@ -102,115 +102,51 @@ const Projects = ({ events }: { events: PubllicEvent[] }) => {
             </div>
             <div className={styles.projectDetails}>
               <div className={styles.projectTexts}>
-                <p className={styles.projectHeading}>Scale Up Conclave 2024</p>
+                <p className={styles.projectHeading}>Dawn Of DPI</p>
                 <p className={styles.projectText}>
-                  The debut of an annual spectacle. Uniting entrepreneurs, investors, professionals,
-                  and business enthusiasts at an electrifying two-day extravaganza!
+                  Bringing together practitioners from diverse backgrounds to share their valuable
+                  experiences in building Digital Public Goods (DPGs) and Digital Public
+                  Infrastructure (DPI) using Open-Source technology.
                 </p>
                 <div className={styles.counts}>
                   <p className={styles.countT}>
-                    <span className={styles.count}>6897 </span>
-                    <span className={styles.countText}>Registrations</span>
-                  </p>
-                  <p className={styles.countT}>
-                    <span className={styles.count}>3394 </span>
-                    <span className={styles.countText}>Check-Ins</span>
+                    <span className={styles.countText}>MakeMyPass </span>
+                    <span className={styles.count}>is now National 🎉</span>
                   </p>
                 </div>
               </div>
               <div className={styles.projectImagesContainer}>
                 <div className={styles.projectImages}>
                   <img
-                    src={sup1}
-                    alt='group of students infornt of makemypass welcome screen at scaleup conclave 2024'
+                    src={dd1}
+                    alt='deepu s nath speaking after checkin using makemypass at in50hours'
                     className={styles.projectImage}
                   />
                   <img
-                    src={sup2}
-                    alt='students using makemypass application for seamless checkin process at scaleup conclave 2024'
+                    src={dd2}
+                    alt='group photo of people who have used makemypass at in50hours'
                     className={styles.projectImage}
                   />
                   <img
-                    src={sup3}
-                    alt='closeup shot of scanning the makemypass ticket using the qr scanner'
-                    className={styles.projectImage}
-                  />
-                </div>
-                <div className={styles.projectImages}>
-                  <img
-                    src={sup1}
-                    alt='group of students infornt of makemypass welcome screen at scaleup conclave 2024'
-                    className={styles.projectImage}
-                  />
-                  <img
-                    src={sup2}
-                    alt='students using makemypass application for seamless checkin process at scaleup conclave 2024'
-                    className={styles.projectImage}
-                  />
-                  <img
-                    src={sup3}
-                    alt='closeup shot of scanning the makemypass ticket using the qr scanner'
-                    className={styles.projectImage}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.project}>
-            <div className={styles.projectDateDay}>
-              <p className={styles.projectDate}>23rd Feb 2024</p>
-              <p className={styles.projectDay}>Friday</p>
-            </div>
-            <div className={styles.lineDot}>
-              <div className={styles.dot}></div>
-              <div className={styles.line}></div>
-            </div>
-            <div className={styles.projectDetails}>
-              <div className={styles.projectTexts}>
-                <p className={styles.projectHeading}>Permute 2024</p>
-                <p className={styles.projectText}>
-                  μLearn’s flagship soiree, unfolded at Park Centre, Technopark, Trivandrum with the
-                  unveiling of μLearn 3.0, the next phase in its much-awaited evolution.
-                </p>
-                <div className={styles.counts}>
-                  <p className={styles.countT}>
-                    <span className={styles.count}>120+ </span>
-                    <span className={styles.countText}>Invites Sent via MakeMyPass</span>
-                  </p>
-                </div>
-              </div>
-              <div className={styles.projectImagesContainer}>
-                <div className={styles.projectImages}>
-                  <img
-                    src={p1}
-                    alt='team makemypass being recognized as the ticketing partner for permute 2024'
-                    className={styles.projectImage}
-                  />
-                  <img
-                    src={p2}
-                    alt='a glimpse of people who checkedin using makemypass tickets'
-                    className={styles.projectImage}
-                  />
-                  <img
-                    src={p3}
-                    alt='a shot showing a studnet showing his makemypass ticket'
+                    src={dd3}
+                    alt='people who have used makemypass at in50hours'
                     className={styles.projectImage}
                   />
                 </div>
                 <div className={styles.projectImages}>
                   <img
-                    src={p1}
-                    alt='team makemypass being recognized as the ticketing partner for permute 2024'
+                    src={dd1}
+                    alt='deepu s nath speaking after checkin using makemypass at in50hours'
                     className={styles.projectImage}
                   />
                   <img
-                    src={p2}
-                    alt='a glimpse of people who checkedin using makemypass tickets'
+                    src={dd2}
+                    alt='group photo of people who have used makemypass at in50hours'
                     className={styles.projectImage}
                   />
                   <img
-                    src={p3}
-                    alt='a shot showing a studnet showing his makemypass ticket'
+                    src={dd3}
+                    alt='people who have used makemypass at in50hours'
                     className={styles.projectImage}
                   />
                 </div>
@@ -280,8 +216,69 @@ const Projects = ({ events }: { events: PubllicEvent[] }) => {
           </div>
           <div className={styles.project}>
             <div className={styles.projectDateDay}>
-              <p className={styles.projectDate}>14th March 2024</p>
-              <p className={styles.projectDay}>Thursday</p>
+              <p className={styles.projectDate}>23rd Feb 2024</p>
+              <p className={styles.projectDay}>Friday</p>
+            </div>
+            <div className={styles.lineDot}>
+              <div className={styles.dot}></div>
+              <div className={styles.line}></div>
+            </div>
+            <div className={styles.projectDetails}>
+              <div className={styles.projectTexts}>
+                <p className={styles.projectHeading}>Permute 2024</p>
+                <p className={styles.projectText}>
+                  μLearn’s flagship soiree, unfolded at Park Centre, Technopark, Trivandrum with the
+                  unveiling of μLearn 3.0, the next phase in its much-awaited evolution.
+                </p>
+                <div className={styles.counts}>
+                  <p className={styles.countT}>
+                    <span className={styles.count}>120+ </span>
+                    <span className={styles.countText}>Invites Sent via MakeMyPass</span>
+                  </p>
+                </div>
+              </div>
+              <div className={styles.projectImagesContainer}>
+                <div className={styles.projectImages}>
+                  <img
+                    src={p1}
+                    alt='team makemypass being recognized as the ticketing partner for permute 2024'
+                    className={styles.projectImage}
+                  />
+                  <img
+                    src={p2}
+                    alt='a glimpse of people who checkedin using makemypass tickets'
+                    className={styles.projectImage}
+                  />
+                  <img
+                    src={p3}
+                    alt='a shot showing a studnet showing his makemypass ticket'
+                    className={styles.projectImage}
+                  />
+                </div>
+                <div className={styles.projectImages}>
+                  <img
+                    src={p1}
+                    alt='team makemypass being recognized as the ticketing partner for permute 2024'
+                    className={styles.projectImage}
+                  />
+                  <img
+                    src={p2}
+                    alt='a glimpse of people who checkedin using makemypass tickets'
+                    className={styles.projectImage}
+                  />
+                  <img
+                    src={p3}
+                    alt='a shot showing a studnet showing his makemypass ticket'
+                    className={styles.projectImage}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.project}>
+            <div className={styles.projectDateDay}>
+              <p className={styles.projectDate}>2-3rd Feb 2024</p>
+              <p className={styles.projectDay}>Friday</p>
             </div>
             <div className={styles.lineDot}>
               <div className={styles.dot}></div>
@@ -294,51 +291,54 @@ const Projects = ({ events }: { events: PubllicEvent[] }) => {
             </div>
             <div className={styles.projectDetails}>
               <div className={styles.projectTexts}>
-                <p className={styles.projectHeading}>Dawn Of DPI</p>
+                <p className={styles.projectHeading}>Scale Up Conclave 2024</p>
                 <p className={styles.projectText}>
-                  Bringing together practitioners from diverse backgrounds to share their valuable
-                  experiences in building Digital Public Goods (DPGs) and Digital Public
-                  Infrastructure (DPI) using Open-Source technology.
+                  The debut of an annual spectacle. Uniting entrepreneurs, investors, professionals,
+                  and business enthusiasts at an electrifying two-day extravaganza!
                 </p>
                 <div className={styles.counts}>
                   <p className={styles.countT}>
-                    <span className={styles.countText}>MakeMyPass </span>
-                    <span className={styles.count}>is now National 🎉</span>
+                    <span className={styles.count}>6897 </span>
+                    <span className={styles.countText}>Registrations</span>
+                  </p>
+                  <p className={styles.countT}>
+                    <span className={styles.count}>3394 </span>
+                    <span className={styles.countText}>Check-Ins</span>
                   </p>
                 </div>
               </div>
               <div className={styles.projectImagesContainer}>
                 <div className={styles.projectImages}>
                   <img
-                    src={dd1}
-                    alt='deepu s nath speaking after checkin using makemypass at in50hours'
+                    src={sup1}
+                    alt='group of students infornt of makemypass welcome screen at scaleup conclave 2024'
                     className={styles.projectImage}
                   />
                   <img
-                    src={dd2}
-                    alt='group photo of people who have used makemypass at in50hours'
+                    src={sup2}
+                    alt='students using makemypass application for seamless checkin process at scaleup conclave 2024'
                     className={styles.projectImage}
                   />
                   <img
-                    src={dd3}
-                    alt='people who have used makemypass at in50hours'
+                    src={sup3}
+                    alt='closeup shot of scanning the makemypass ticket using the qr scanner'
                     className={styles.projectImage}
                   />
                 </div>
                 <div className={styles.projectImages}>
                   <img
-                    src={dd1}
-                    alt='deepu s nath speaking after checkin using makemypass at in50hours'
+                    src={sup1}
+                    alt='group of students infornt of makemypass welcome screen at scaleup conclave 2024'
                     className={styles.projectImage}
                   />
                   <img
-                    src={dd2}
-                    alt='group photo of people who have used makemypass at in50hours'
+                    src={sup2}
+                    alt='students using makemypass application for seamless checkin process at scaleup conclave 2024'
                     className={styles.projectImage}
                   />
                   <img
-                    src={dd3}
-                    alt='people who have used makemypass at in50hours'
+                    src={sup3}
+                    alt='closeup shot of scanning the makemypass ticket using the qr scanner'
                     className={styles.projectImage}
                   />
                 </div>
