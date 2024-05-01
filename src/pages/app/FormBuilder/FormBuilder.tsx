77cb27da05c75769b306fdfd8ddf12d837a8eb68
Aswin <aswinasokofficial@gmail.com>
2024-05-01 13:24:57 +0530

    feat(form-builder):inital-ui-built

--- src/pages/app/FormBuilder/FormBuilder.tsx
+++ src/pages/app/FormBuilder/FormBuilder.tsx
@@ -1,4 +1,4 @@
-import { FaAddressCard, FaUser } from 'react-icons/fa6';
+import { FaAddressCard, FaRegEyeSlash, FaUser } from 'react-icons/fa6';
 import EventHeader from '../../../components/EventHeader/EventHeader';
 import Glance from '../../../components/Glance/Glance';
 import Theme from '../../../components/Theme/Theme';
@@ -8,6 +8,11 @@ import Slider from '../../../components/SliderButton/Slider';
 import { BsAlphabetUppercase } from 'react-icons/bs';
 import { RxDragHandleDots2 } from 'react-icons/rx';
 import { LuPencil } from 'react-icons/lu';
+import { RiDeleteBinLine } from 'react-icons/ri';
+import Select from 'react-select';
+import { customStyles } from '../EventPage/constants';
+
+const categories = ['Attendee', 'Speaker', 'Sponsor', 'Exhibitor', 'Staff'];
 
 const FormBuilder = () => {
   return (
@@ -88,6 +93,235 @@ const FormBuilder = () => {
                   </div>
                   <LuPencil size={20} color='#606264' />
                 </div>
+
+                <div className={styles.customFieldExp}>
+                  <div className={styles.row}>
+                    <div className={styles.row1}>
+                      <RxDragHandleDots2 size={25} color='#606264' />
+
+                      <p className={styles.customFieldLabel}>Phone Number</p>
+                    </div>
+                    <div className={styles.expandedRight}>
+                      <div className={styles.requiredCheckbox}>
+                        Required
+                        <Slider checked={true} text={''} onChange={() => {}} />
+                      </div>
+                      <FaRegEyeSlash size={25} color='#606264' />
+                    </div>
+                  </div>
+
+                  <div className={styles.customFieldName}>
+                    <input type='text' placeholder='Field Name' />
+                  </div>
+                  <div className={styles.customFieldName}>
+                    <input type='text' placeholder='Add Some help text.' />
+                  </div>
+
+                  <div
+                    className={styles.row1}
+                    style={{
+                      marginTop: '1rem',
+                      marginLeft: '1rem',
+                    }}
+                  >
+                    <Slider checked={true} text={''} onChange={() => {}} />
+                    <p className={styles.customFieldLabel}>
+                      Show Field only when the conditions are met.
+                    </p>
+                  </div>
+
+                  <div className={styles.conditions}>
+                    <div className={styles.conditionRow}>
+                      <p className={styles.when}>When</p>
+                      <div className={styles.conditionsSelect}>
+                        <Select
+                          className='basic-single'
+                          classNamePrefix='select'
+                          name='role'
+                          options={[
+                            ...categories.map((category) => ({
+                              value: category,
+                              label: category,
+                            })),
+                            {
+                              value: '',
+                              label: 'All',
+                            },
+                          ]}
+                          styles={{
+                            ...customStyles,
+                            control: (provided: any) => ({
+                              ...provided,
+                              border: 'none',
+                              backgroundColor: 'transparent', // Set the background color to transparent
+                              color: '#fff',
+                              fontFamily: 'Inter, sans-serif',
+                              fontStyle: 'normal',
+                              fontWeight: 400,
+                              fontSize: '0.9rem',
+                              zIndex: 1000,
+                              width: '12rem',
+                            }),
+                          }}
+                        />
+                        <Select
+                          className='basic-single'
+                          classNamePrefix='select'
+                          name='role'
+                          options={[
+                            ...categories.map((category) => ({
+                              value: category,
+                              label: category,
+                            })),
+                            {
+                              value: '',
+                              label: 'All',
+                            },
+                          ]}
+                          styles={{
+                            ...customStyles,
+                            control: (provided: any) => ({
+                              ...provided,
+                              border: 'none',
+                              backgroundColor: 'transparent', // Set the background color to transparent
+                              color: '#fff',
+                              fontFamily: 'Inter, sans-serif',
+                              fontStyle: 'normal',
+                              fontWeight: 400,
+                              fontSize: '0.9rem',
+                              zIndex: 1000,
+                              width: '12rem',
+                            }),
+                          }}
+                        />
+                        <input type='text' placeholder='Enter a Value' />
+
+                        <RiDeleteBinLine size={20} color='#606264' />
+                        <RxDragHandleDots2
+                          style={{
+                            marginLeft: '1rem',
+                          }}
+                          size={20}
+                          color='#606264'
+                        />
+                      </div>
+                    </div>
+
+                    <div className={styles.conditionRow}>
+                      <Select
+                        className='basic-single'
+                        classNamePrefix='select'
+                        name='role'
+                        options={[
+                          ...categories.map((category) => ({
+                            value: category,
+                            label: category,
+                          })),
+                          {
+                            value: '',
+                            label: 'All',
+                          },
+                        ]}
+                        styles={{
+                          ...customStyles,
+                          control: (provided: any) => ({
+                            ...provided,
+                            border: 'none',
+                            backgroundColor: '#24332F', // Set the background color to transparent
+                            color: '#fff',
+                            fontFamily: 'Inter, sans-serif',
+                            fontStyle: 'normal',
+                            fontWeight: 400,
+                            fontSize: '0.9rem',
+                            zIndex: 1000,
+                            width: '8rem',
+                          }),
+                        }}
+                      />
+                      <div className={styles.conditionsSelect}>
+                        <Select
+                          className='basic-single'
+                          classNamePrefix='select'
+                          name='role'
+                          options={[
+                            ...categories.map((category) => ({
+                              value: category,
+                              label: category,
+                            })),
+                            {
+                              value: '',
+                              label: 'All',
+                            },
+                          ]}
+                          styles={{
+                            ...customStyles,
+                            control: (provided: any) => ({
+                              ...provided,
+                              border: 'none',
+                              backgroundColor: 'transparent', // Set the background color to transparent
+                              color: '#fff',
+                              fontFamily: 'Inter, sans-serif',
+                              fontStyle: 'normal',
+                              fontWeight: 400,
+                              fontSize: '0.9rem',
+                              zIndex: 1000,
+                              width: '12rem',
+                            }),
+                          }}
+                        />
+                        <Select
+                          className='basic-single'
+                          classNamePrefix='select'
+                          name='role'
+                          options={[
+                            ...categories.map((category) => ({
+                              value: category,
+                              label: category,
+                            })),
+                            {
+                              value: '',
+                              label: 'All',
+                            },
+                          ]}
+                          styles={{
+                            ...customStyles,
+                            control: (provided: any) => ({
+                              ...provided,
+                              border: 'none',
+                              backgroundColor: 'transparent', // Set the background color to transparent
+                              color: '#fff',
+                              fontFamily: 'Inter, sans-serif',
+                              fontStyle: 'normal',
+                              fontWeight: 400,
+                              fontSize: '0.9rem',
+                              zIndex: 1000,
+                              width: '12rem',
+                            }),
+                          }}
+                        />
+                        <input type='text' placeholder='Enter a Value' />
+
+                        <RiDeleteBinLine size={20} color='#606264' />
+                        <RxDragHandleDots2
+                          style={{
+                            marginLeft: '1rem',
+                          }}
+                          size={20}
+                          color='#606264'
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <p className={styles.addCustomField}>
+                    <span>+</span> Add Custom Field
+                  </p>
+                </div>
+
+                <button className={styles.addQuestionButton}>
+                  <span>+</span>
+                  {''}Add Question
+                </button>
               </div>
             </div>
           </div>
