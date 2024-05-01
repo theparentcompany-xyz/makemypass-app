c89a6c90613e9b11d4988f7ac467530ce3b0c115
Aswin <aswinasokofficial@gmail.com>
2024-05-01 08:29:26 +0530

    fix(build)

--- services/RoleChecker.tsx
+++ services/RoleChecker.tsx
@@ -1,4 +1,3 @@
-import toast from 'react-hot-toast';
 import { Roles } from './types';
 import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { getEventId } from '../src/apis/events';
@@ -7,14 +6,10 @@ const RoleChecker = ({
   redirectPath,
   children,
   roles,
-  toastTitle,
-  toastDescription,
 }: {
   redirectPath?: JSX.Element;
   children: JSX.Element;
   roles: Roles[];
-  toastTitle?: string;
-  toastDescription?: string;
 }) => {
   const currentTitle = JSON.parse(sessionStorage.getItem('eventData')!)?.event_name;
   const currentUserRole = [JSON.parse(sessionStorage.getItem('eventData')!)?.current_user_role];
