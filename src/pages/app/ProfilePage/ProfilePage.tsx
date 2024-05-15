bee2199c7285c3cd4d1e9434f361777aa97205c3
BlazeSN860 <virajprabhup@gmail.com>
2024-05-16 03:35:59 +0530

    fix(ProfilePage): added-loader-feature

--- src/pages/app/ProfilePage/ProfilePage.tsx
+++ src/pages/app/ProfilePage/ProfilePage.tsx
@@ -3,22 +3,22 @@ import styles from './ProfilePage.module.css';
 import Theme from '../../../components/Theme/Theme';
 import { setUserData, updateProfile, getProfileInfo } from '../../../apis/user';
 import { useLocation } from 'react-router-dom';
+import ButtonLoader from '../../../components/ButtonLoader/ButtonLoader';
 
 const ProfilePage = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const token = queryParams.get('token')?.replace(/\/+$/, '') as string;
-
   const NameRef = useRef<HTMLInputElement>(null);
   const EmailRef = useRef<HTMLInputElement>(null);
-
+  const [loading, setLoading] = React.useState(false);
   const handleUpdateProfile = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.target as HTMLFormElement);
     if (token) {
-      setUserData({ formData, token });
+      setUserData({ formData, token, setLoading });
     } else {
-      updateProfile({ data: formData });
+      updateProfile({ data: formData }, setLoading);
     }
   };
 
@@ -60,8 +60,10 @@ const ProfilePage = () => {
               <input type='file' id='profile_pic' name='profile_pic' className={styles.fileInput} />
             </div>
             <button type='submit' className={styles.updateButton}>
-              Update details
+              <span className={styles.buttonText}>Update details</span>
+              <ButtonLoader loading={loading} />
             </button>
+
             <div className={styles.formGroup}>
               <label className={styles.optional}>All fields are optional</label>
             </div>
