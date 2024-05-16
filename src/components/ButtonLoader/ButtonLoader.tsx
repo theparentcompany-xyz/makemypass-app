7bc2b418935675a380c2f7603f2044954143a4a4
BlazeSN860 <virajprabhup@gmail.com>
2024-05-16 17:35:18 +0530

    chore(ButtonLoader): removed-comments.

--- src/components/ButtonLoader/ButtonLoader.tsx
+++ src/components/ButtonLoader/ButtonLoader.tsx
@@ -1,6 +1,4 @@
-import {
-  ClipLoader,
-} from 'react-spinners';
+import { ClipLoader } from 'react-spinners';
 import styles from './ButtonLoader.module.css';
 type Props = {
   loading?: boolean | undefined;
@@ -9,15 +7,6 @@ type Props = {
 const LoaderButton = ({ loading }: Props) => {
   return (
     <div className={`${styles.loaderButton} ${loading ? styles.loading : ''}`}>
-      {/* <ColorRing
-        visible={true}
-        height='15'
-        width='20'
-        ariaLabel='blocks-loading'
-        wrapperStyle={{}}
-        wrapperClass='blocks-wrapper'
-        colors={['f3f3f3', 'f3f3f3', 'f3f3f3', 'f3f3f3', 'f3f3f3']}
-      /> */}
       <ClipLoader color='white' loading={true} size={10} aria-label='Loading Spinner' />
     </div>
   );
