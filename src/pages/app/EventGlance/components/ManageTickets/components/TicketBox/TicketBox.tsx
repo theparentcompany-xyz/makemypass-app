096814d825c7de311e7f12734d6253998b34e650
BlazeSN860 <virajprabhup@gmail.com>
2024-06-03 22:21:38 +0530

    feat(TicketBox): added-ticket-boxes.

---
+++ src/pages/app/EventGlance/components/ManageTickets/components/TicketBox/TicketBox.tsx
@@ -0,0 +1,38 @@
+import styles from './TicketBox.module.css';
+import Slider from '../../../../../../../components/SliderButton/Slider';
+import { TicketType } from '../../../../../../../apis/types';
+
+type Props = {
+  ticketInfo: TicketType;
+  onClick: () => void;
+  selected: boolean;
+};
+
+const TicketBox = ({ ticketInfo, onClick, selected }: Props) => {
+  return (
+    <>
+      <div className={`${styles.ticketBox} ${selected ? styles.selected : ''}`} onClick={onClick}>
+        <div className={styles.ticket}></div>
+        <div className={styles.ticketInfo}>
+          <p className={styles.ticketName}>{ticketInfo.title}</p>
+          <p className={styles.ticketType}>
+            1000 / {ticketInfo.capacity || ' Unlimited'} <br />
+            Registered
+          </p>
+        </div>
+        <div className={styles.ticketFooter}>
+          <div className={styles.ticketFooterLeft}>
+            {' '}
+            <span className={styles.availableDot}></span>Available
+          </div>
+          <div className={styles.ticketFooterRight}>
+            <label className={styles.closeTicketLabel}>Close Ticket</label>
+            <Slider checked={true} onChange={() => {}} sliderStyle={{ transform: 'scale(0.7)' }} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TicketBox;
