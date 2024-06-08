3d021319c0863c02a02c511da42866ab352ec96e
BlazeSN860 <virajprabhup@gmail.com>
2024-06-09 02:12:07 +0530

    fix(mails): set-mails.

--- src/apis/mails.ts
+++ src/apis/mails.ts
@@ -1,9 +1,12 @@
+import React from "react";
 import { privateGateway } from "../../services/apiGateway";
 import { makeMyPass } from "../../services/urls";
+import { MailType } from "./types";
 
-export const listMails = (eventId: string) => {
+export const listMails = (eventId: string, setMails: React.Dispatch<React.SetStateAction<MailType[]>>) => {
     privateGateway.get(makeMyPass.listMails(eventId))
     .then(response => {
+        setMails(response.data.response)
         console.log( response.data.response)
     }).catch(error => {
         console.log(error)
