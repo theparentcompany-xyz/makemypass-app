fa5b4ca51bf39fe44d61e2ab1e5b5e8e09ad7ddd
BlazeSN860 <virajprabhup@gmail.com>
2024-06-09 13:32:17 +0530

    feat(mails): added-update-api.

--- src/apis/mails.ts
+++ src/apis/mails.ts
@@ -1,24 +1,48 @@
-import React from "react";
-import { privateGateway } from "../../services/apiGateway";
-import { makeMyPass } from "../../services/urls";
-import { MailType } from "./types";
+import React from 'react';
+import { privateGateway } from '../../services/apiGateway';
+import { makeMyPass } from '../../services/urls';
+import { MailType } from './types';
+import toast from 'react-hot-toast';
 
-export const listMails = (eventId: string, setMails: React.Dispatch<React.SetStateAction<MailType[]>>) => {
-    privateGateway.get(makeMyPass.listMails(eventId))
-    .then(response => {
-        setMails(response.data.response)
-        console.log( response.data.response)
-    }).catch(error => {
-        console.log(error)
+export const listMails = (
+  eventId: string,
+  setMails: React.Dispatch<React.SetStateAction<MailType[]>>,
+) => {
+  privateGateway
+    .get(makeMyPass.listMails(eventId))
+    .then((response) => {
+      setMails(response.data.response);
+      console.log(response.data.response);
     })
+    .catch((error) => {
+      console.log(error);
+    });
 };
- 
 
 export const getMail = (eventId: string, mailId: string) => {
-    privateGateway.get(makeMyPass.getMail(eventId, mailId))
-    .then(response => {
-        console.log( response.data.response)
-    }).catch(error => {
-        console.log(error)
+  privateGateway
+    .get(makeMyPass.getMail(eventId, mailId))
+    .then((response) => {
+      console.log(response.data.response);
     })
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const updateMail = (eventId: string, mailId: string, data: Record<string, any>) => {
+  privateGateway
+    .patch(makeMyPass.updateMail(eventId, mailId), data, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+    .then((response) => {
+      toast.success('Mail updated successfully');
+      console.log(response.data.response);
+    })
+    .catch((error) => {
+      toast.error('Error while updating mail');
+      console.log(error);
+    });
+};
