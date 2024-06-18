6b92c8368356b979edfba92b82078dfb7a6917ab
Aswin Asok <aswinasokofficial@gmail.com>
2024-06-18 15:52:09 +0530

    fix-url

--- services/urls.ts
+++ services/urls.ts
@@ -122,7 +122,7 @@ export const makeMyPass = {
   //Mail URLS
   listMails: (eventId: string) => makeMyPassURL(`/event/${eventId}/communication/list-event-mail/`),
   sentTestMail: (eventId: string, mailId: string) =>
-    makeMyPassURL(`/event/${eventId}/send-test-mail/${mailId}`),
+    makeMyPassURL(`/event/${eventId}/communication/send-test-mail/${mailId}`),
   getMail: (eventId: string, mailId: string) =>
     makeMyPassURL(`/event/${eventId}/communication/get-event-mail/${mailId}/`),
   updateMail: (eventId: string, mailId: string) =>
