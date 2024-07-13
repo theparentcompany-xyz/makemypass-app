4d1951358cecf4d17b027ef69f443179a6456f0f
Aswin Asok <aswinasokofficial@gmail.com>
2024-07-13 15:10:49 +0530

    fix(bulkImport)

--- src/pages/app/Guests/components/BulkUpload/BulkUpload.tsx
+++ src/pages/app/Guests/components/BulkUpload/BulkUpload.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
-import { getCSVTemplate, getFileStatus, uploadFile } from '../../../../../apis/guests';
+import {
+  downloadFile,
+  getCSVTemplate,
+  getFileStatus,
+  uploadFile,
+} from '../../../../../apis/guests';
 import { TicketType } from '../../../../../apis/types';
 import Modal from '../../../../../components/Modal/Modal';
 import { customStyles } from '../../../EventPage/constants';
@@ -7,18 +12,25 @@ import styles from './BulkUpload.module.css';
 import Select from 'react-select';
 import { getTickets } from '../../../../../apis/tickets';
 import toast from 'react-hot-toast';
+import { formatDate } from '../../../../../common/commonFunctions';
+import { BiSolidReport } from 'react-icons/bi';
+import { BulkUploadType } from './types';
 
 const BulkUpload = ({ onClose }: { onClose: () => void }) => {
   const { event_id: eventId } = JSON.parse(sessionStorage.getItem('eventData')!);
   const [tickets, setTickets] = useState<TicketType[]>([]);
   const [selectedTickets, setSelectedTickets] = useState<string[]>([]);
   const [file, setFile] = useState<File | null>(null);
-  const [fileStatus, setFileStatus] = useState<any[]>([]);
+  const [fileStatus, setFileStatus] = useState<BulkUploadType[]>([]);
 
   useEffect(() => {
     getTickets(eventId, setTickets);
     getFileStatus(eventId, setFileStatus);
   }, []);
+
+  const generateCSVReport = (fileId: string) => {
+    downloadFile(eventId, fileId, 'completed');
+  };
 
   return (
     <Modal title='Bulk Upload' onClose={onClose} type='center'>
@@ -65,11 +77,24 @@ const BulkUpload = ({ onClose }: { onClose: () => void }) => {
           <div className={styles.log} key={index}>
             <div className={styles.logDetails}>
               <p className={styles.logName}>
-                {file.name} <span className={styles.logDate}>29th Feb 2023, 19:34 IST</span>
+                {file.filename ?? 'CSV File'}{' '}
+                <span className={styles.logDate}>{formatDate(file.created_at, true)}</span>
               </p>
-              <p className={styles.logUploadCount}>1000/1299 Uploaded</p>
+              <div className={styles.fileStatus}>
+                <p className={styles.success}>{file.success_count} Success</p>
+                <p className={styles.error}>{file.failure_count} Failed</p>
+                <p className={styles.total}>{file.total_count} Total</p>
+              </div>
             </div>
-            <p className={styles.logStatus}>Finished</p>
+            <p className={styles.logStatus}>{file.status}</p>
+            <BiSolidReport
+              onClick={() => {
+                generateCSVReport(file.file_id);
+              }}
+              title='Download Report'
+              color='#8e8e8e'
+              className={styles.reportIcon}
+            />
           </div>
         ))}
         {fileStatus.length === 0 && <p className={styles.noLogs}>No logs available</p>}
---
+++ src/pages/app/Guests/components/BulkUpload/types.ts
@@ -0,0 +1,11 @@
+export type BulkUploadType = {
+  filename: string;
+  file_id: string;
+  status: string;
+  success_count: number;
+  failure_count: number;
+  total_count: number;
+  created_by: string;
+  created_at: string;
+  updated_at: string;
+};
