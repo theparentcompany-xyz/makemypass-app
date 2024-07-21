b081553ef3b78e740a39fa90172edc60ee687771
Aswin Asok <aswinasokofficial@gmail.com>
2024-07-21 17:15:31 +0530

    feat(logs):event-mail-log-added

--- src/apis/logs.ts
+++ src/apis/logs.ts
@@ -1,19 +1,19 @@
 import { Dispatch } from 'react';
 import { privateGateway } from '../../services/apiGateway';
 import { makeMyPass } from '../../services/urls';
+import { EmailType } from '../pages/app/Guests/components/ViewGuest/types';
 
 export const getAllMailLog = async (
   eventId: string,
-  setAllMailLog: Dispatch<React.SetStateAction<boolean>>,
+  setAllMailLog: Dispatch<React.SetStateAction<EmailType[]>>,
 ) => {
   privateGateway
     .get(makeMyPass.getAllMailLog(eventId))
     .then((response) => {
       console.log(response.data.response);
-      setAllMailLog(true);
+      setAllMailLog(response.data.response);
     })
     .catch((error) => {
       console.log(error);
-      setAllMailLog(true);
     });
 };
