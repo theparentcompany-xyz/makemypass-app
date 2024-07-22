b30d362566e37a310495604b00d5bf866808e8be
Aswin Asok <aswinasokofficial@gmail.com>
2024-07-22 21:25:44 +0530

    fix(logs):fixed-css-issues

--- src/pages/app/EventLogs/EventLogs.tsx
+++ src/pages/app/EventLogs/EventLogs.tsx
@@ -37,49 +37,46 @@ const EventLogs = () => {
     <>
       <Theme>
         <EventHeader previousPageNavigate='/events' />
-        <Glance tab='Logs' />
-        <p className={styles.mailLogsHeaders}>Mail logs</p>
-        <div className={styles.mailsContainer}>
-          {mailLogs.map((mail, index) => (
-            <div className={styles.mail} key={index}>
-              <div className={styles.expandIcon}>
-                {
-                  <BiChevronDown
-                    onClick={() => toggleMailContent(mail.id)}
-                    size={25}
-                    style={{
-                      transform: mail.show_content ? 'rotate(180deg)' : 'rotate(0deg)',
-                    }}
-                  />
-                }
-              </div>
+        <Glance tab='logs' />
+        <div className={styles.mailsOuterContainer}>
+          <p className={styles.mailLogsHeaders}>
+            {mailLogs.length === 0 ? 'No logs available' : 'Mail Logs'}
+          </p>
+          <div className={styles.mailsContainer}>
+            {mailLogs.map((mail, index) => (
+              <div className={styles.mail} key={index}>
+                <div className={styles.expandIcon}>
+                  {
+                    <BiChevronDown
+                      onClick={() => toggleMailContent(mail.id)}
+                      size={25}
+                      style={{
+                        transform: mail.show_content ? 'rotate(180deg)' : 'rotate(0deg)',
+                      }}
+                    />
+                  }
+                </div>
 
-              <div className={styles.mailHeader}>
-                <div className={styles.mailHeaderContents}>
-                  <p className={styles.mailType}>
-                    {mail.type} Mail @ {formatDate(mail.created_at, true)}
-                  </p>
-                  <p className={styles.mailSubject}>{mail.subject}</p>
+                <div className={styles.mailHeader}>
+                  <div className={styles.mailHeaderContents}>
+                    <p className={styles.mailType}>
+                      {mail.type} Mail @ {formatDate(mail.created_at, true)}
+                    </p>
+                    <p className={styles.mailSubject}>{mail.subject}</p>
 
-                  <p className={styles.mailDescription}>
-                    To: <span>{mail.send_to}</span> <br />
-                    From: <span>{mail.send_from}</span>
-                  </p>
+                    <p className={styles.mailDescription}>
+                      To: <span>{mail.send_to}</span> <br />
+                      From: <span>{mail.send_from}</span>
+                    </p>
+                  </div>
+                </div>
+                {mail.show_content && <hr className={styles.line} />}
+                <div className={styles.mailContent}>
+                  <pre>{mail.show_content && mail.body}</pre>
                 </div>
               </div>
-              <hr className={styles.line} />
-              <div className={styles.mailContent}>
-                <pre>
-                  {mail.show_content
-                    ? mail.body
-                    : mail.body.length > 100
-                      ? mail.body.substring(0, 100) + '...'
-                      : mail.body}
-                </pre>
-                {/* <div className={styles.attachment}>Ticket.png</div> */}
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </Theme>
     </>
