1f525480d95f08e6eda157f49a17923487d9f60a
M Viraj Prabhu <virajprabhup@gmail.com>
2024-07-31 20:03:46 +0530

    fix(ProfilePage): added-update-password

--- src/pages/app/ProfilePage/ProfilePage.tsx
+++ src/pages/app/ProfilePage/ProfilePage.tsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './ProfilePage.module.css';
 import Theme from '../../../components/Theme/Theme';
-import { setUserData, updateProfile, getProfileInfo } from '../../../apis/user';
+import {
+  setUserData,
+  updateProfile,
+  getProfileInfo,
+  updateProfilePassword,
+} from '../../../apis/user';
 import { useLocation } from 'react-router-dom';
 import { FiEdit3 } from 'react-icons/fi';
 import EventBox from './components/EventBox/EventBox';
@@ -13,6 +18,8 @@ import { Event } from '../../../apis/types';
 import ButtonLoader from '../../../components/ButtonLoader/ButtonLoader';
 import { FaCamera } from 'react-icons/fa';
 import { BiUser } from 'react-icons/bi';
+import { BsEye, BsEyeSlash } from 'react-icons/bs';
+import toast from 'react-hot-toast';
 
 const ProfilePage = () => {
   const location = useLocation();
@@ -36,6 +43,9 @@ const ProfilePage = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   // const [currentTab, setCurrentTab] = useState('owner');
   const [eventsData, setEventsData] = useState<Event[]>([]);
+  const [showCurrentPassword, setShowCurrentPassword] = useState<boolean>(false);
+  const [showNewPassword, setShowNewPassword] = useState<boolean>(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
   // const tabs: TabsType = {
   //   owner: {
   //     title: 'Your Events',
@@ -59,6 +69,18 @@ const ProfilePage = () => {
     } else {
       updateProfile({ data: formData }, setLoading);
     }
+  };
+
+  const handleUpdatePassword = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+
+    if (formData.get('new_password') !== formData.get('confirm_new_password')) {
+      toast.error('Passwords do not match');
+      return;
+    }
+
+    updateProfilePassword({ data: formData }, setLoading);
   };
 
   useEffect(() => {
@@ -92,7 +114,7 @@ const ProfilePage = () => {
           <Theme>
             {isOpenModal && (
               <Modal type='side' onClose={() => setIsOpenModal(false)}>
-                <div className={styles.viewProfile}>
+                <div className={styles.modalContainer}>
                   <form className={styles.profileForm} onSubmit={handleUpdateProfile}>
                     <div className={styles.userDetailsContainer}>
                       {user?.profile_pic?.includes('.png') && !editUser?.profile_pic && (
@@ -145,56 +167,126 @@ const ProfilePage = () => {
                         <label className={styles.modalUserMail}>{editUser?.email}</label>
                       </div>
                     </div>
-                    <div className={styles.formGroup}>
-                      <label htmlFor='name'>Full Name</label>
-                      <input
-                        type='text'
-                        id='name'
-                        name='name'
-                        autoComplete='off'
-                        onChange={(e) => setEditUser({ ...editUser, name: e.target.value })}
-                        value={editUser?.name}
-                      />
-                    </div>
-                    {token ? (
+                    <label>Personal Information</label>
+                    <div className={styles.viewProfile}>
                       <div className={styles.formGroup}>
-                        <label htmlFor='password'>Password</label>
-                        <input type='password' id='password' name='password' />
-                      </div>
-                    ) : (
-                      <div className={styles.formGroup}>
-                        <label htmlFor='email'>Email Address</label>
+                        <label htmlFor='name'>Full Name</label>
                         <input
-                          required
-                          type='email'
-                          id='email'
-                          name='email'
-                          value={editUser?.email}
-                          onChange={(e) => setEditUser({ ...editUser, email: e.target.value })}
-                          disabled={true}
+                          type='text'
+                          id='name'
+                          name='name'
+                          autoComplete='off'
+                          onChange={(e) => setEditUser({ ...editUser, name: e.target.value })}
+                          value={editUser?.name}
                         />
                       </div>
-                    )}
+                      {token ? (
+                        <div className={styles.formGroup}>
+                          <label htmlFor='password'>Password</label>
+                          <input type='password' id='password' name='password' />
+                        </div>
+                      ) : (
+                        <div className={styles.formGroup}>
+                          <label htmlFor='email'>Email Address</label>
+                          <input
+                            required
+                            type='email'
+                            id='email'
+                            name='email'
+                            value={editUser?.email}
+                            onChange={(e) => setEditUser({ ...editUser, email: e.target.value })}
+                            disabled={true}
+                          />
+                        </div>
+                      )}
+                      <div className={styles.formGroup}>
+                        <input
+                          type='file'
+                          id='profile_pic'
+                          name='profile_pic'
+                          className={styles.fileInput}
+                          onChange={(e) =>
+                            setEditUser({ ...editUser, profile_pic: e.target?.files?.[0] })
+                          }
+                          ref={ProfilePicRef}
+                        />
+                      </div>
+                      <button type='submit' className={styles.updateButton}>
+                        <span className={styles.buttonText}>Update details</span>
+                        <ButtonLoader loading={loading} />
+                      </button>
+
+                      <div className={styles.formGroup}>
+                        <label className={styles.optional}>All fields are optional</label>
+                      </div>
+                    </div>
+                  </form>
+
+                  <form className={styles.viewProfile} onSubmit={handleUpdatePassword}>
                     <div className={styles.formGroup}>
-                      <input
-                        type='file'
-                        id='profile_pic'
-                        name='profile_pic'
-                        className={styles.fileInput}
-                        onChange={(e) =>
-                          setEditUser({ ...editUser, profile_pic: e.target?.files?.[0] })
-                        }
-                        ref={ProfilePicRef}
-                      />
+                      <label htmlFor='current_password'>Current Password</label>
+                      <div className={styles.passwordContainer}>
+                        <input
+                          type={showCurrentPassword ? 'text' : 'password'}
+                          id='current_password'
+                          name='current_password'
+                          style={{
+                            backgroundColor: 'transparent',
+                            width: '100%',
+                            borderRadius: '0',
+                            padding: '0',
+                          }}
+                          required
+                        />
+                        <div onClick={() => setShowCurrentPassword((prevPass) => !prevPass)}>
+                          {showCurrentPassword ? <BsEye /> : <BsEyeSlash />}
+                        </div>
+                      </div>
+                    </div>
+                    <div className={styles.formGroup}>
+                      <label htmlFor='new_password'>New Password</label>
+                      <div className={styles.passwordContainer}>
+                        <input
+                          type={showNewPassword ? 'text' : 'password'}
+                          id='new_password'
+                          name='new_password'
+                          style={{
+                            backgroundColor: 'transparent',
+                            width: '100%',
+                            borderRadius: '0',
+                            padding: '0',
+                          }}
+                          required
+                        />
+                        <div onClick={() => setShowNewPassword((prevPass) => !prevPass)}>
+                          {showNewPassword ? <BsEye /> : <BsEyeSlash />}
+                        </div>
+                      </div>
+                    </div>
+                    <div className={styles.formGroup}>
+                      <label htmlFor='confirm_new_password'>Confirm New Password</label>
+                      <div className={styles.passwordContainer}>
+                        <input
+                          type={showConfirmPassword ? 'text' : 'password'}
+                          id='confirm_new_password'
+                          name='confirm_new_password'
+                          style={{
+                            backgroundColor: 'transparent',
+                            width: '100%',
+                            borderRadius: '0',
+                            padding: '0',
+                          }}
+                          required
+                        />
+                        <div onClick={() => setShowConfirmPassword((prevPass) => !prevPass)}>
+                          {showConfirmPassword ? <BsEye /> : <BsEyeSlash />}
+                        </div>
+                      </div>
                     </div>
                     <button type='submit' className={styles.updateButton}>
-                      <span className={styles.buttonText}>Update details</span>
+                      <span className={styles.buttonText}>Update Password</span>
                       <ButtonLoader loading={loading} />
                     </button>
-
-                    <div className={styles.formGroup}>
-                      <label className={styles.optional}>All fields are optional</label>
-                    </div>
                   </form>
                 </div>
               </Modal>
