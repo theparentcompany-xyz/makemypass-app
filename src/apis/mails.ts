299ea18e931aeccdcdff8cfc1f81874f0aa0614f
M Viraj Prabhu <virajprabhup@gmail.com>
2024-08-02 23:13:21 +0530

    fix(mail): changed-getMail-into-async-await.

--- src/apis/mails.ts
+++ src/apis/mails.ts
@@ -41,41 +41,44 @@ export const getMail = (
     });
 };
 
-export const updateMail = (
+export const updateMail = async (
   eventId: string,
   selectedMail: MailType,
   data: Record<string, any>,
   setMails: React.Dispatch<React.SetStateAction<listMailType[]>>,
   setMailData: React.Dispatch<React.SetStateAction<MailType | undefined>>,
 ) => {
-  privateGateway
-    .patch(makeMyPass.updateMail(eventId, selectedMail?.id), data, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
+  try {
+    const response = await privateGateway.patch(
+      makeMyPass.updateMail(eventId, selectedMail?.id),
+      data,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
       },
-    })
-    .then((response) => {
-      setMails((mails) =>
-        mails.map((mail) =>
-          mail.id === selectedMail.id
-            ? {
-                id: response?.data?.response?.id ? response.data.response.id : selectedMail.id,
-                type: selectedMail.type,
-                subject: selectedMail.subject,
-              }
-            : mail,
-        ),
-      );
-      setMailData({
-        ...selectedMail,
-        id: response?.data?.response?.id ? response.data.response.id : selectedMail.id,
-      } as MailType);
+    );
 
-      toast.success(response.data.message.general[0] || 'Mail updated successfully');
-    })
-    .catch((error) => {
-      toast.error(error?.response?.data?.message?.general[0] || 'Error while updating mail');
-    });
+    setMails((mails) =>
+      mails.map((mail) =>
+        mail.id === selectedMail.id
+          ? {
+              id: response?.data?.response?.id ? response.data.response.id : selectedMail.id,
+              type: selectedMail.type,
+              subject: selectedMail.subject,
+            }
+          : mail,
+      ),
+    );
+    setMailData({
+      ...selectedMail,
+      id: response?.data?.response?.id ? response.data.response.id : selectedMail.id,
+    } as MailType);
+
+    toast.success(response.data.message.general[0] || 'Mail updated successfully');
+  } catch (error: any) {
+    toast.error(error?.response?.data?.message?.general[0] || 'Error while updating mail');
+  }
 };
 
 export const getMailService = (
