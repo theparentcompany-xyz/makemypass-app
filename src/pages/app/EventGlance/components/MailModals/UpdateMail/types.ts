d3117a6cff67cb7a140be1dc3ead5046c5956faf
M Viraj Prabhu <virajprabhup@gmail.com>
2024-08-31 23:32:56 +0530

    fix(types): changed-file-route

---
+++ src/pages/app/EventGlance/components/MailModals/UpdateMail/types.ts
@@ -0,0 +1,14 @@
+import { listMailType } from '../../../../../../apis/types';
+
+export type PropTypes = {
+  selectedMail: listMailType | undefined;
+  setSelectedMail: React.Dispatch<React.SetStateAction<listMailType | undefined>>;
+  setCustomMail: React.Dispatch<React.SetStateAction<boolean>>;
+  setMails: React.Dispatch<React.SetStateAction<listMailType[]>>;
+};
+
+export type previewType = {
+  previewURL: string;
+  previewExtension: string;
+  previewName: string;
+};
